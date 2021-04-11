bd97baca8bc1b6892a1b2e1f44568f0d3270f02f
Yashashree113 <yashashreek113@gmail.com>
2021-04-11 12:14:49 +0530

    Committing the js file again

--- sum.js
+++ sum.js
@@ -1,5 +1,4 @@
 const num1=8;
 const num2=5;
-const num3=3;
-const sum=num1+num2+num3;
-console.log('Sum of '+num1+ ','+ 'and'+ num3+ 'is: '+sum);
+const sum=num1+num2;
+console.log('Sum of '+num1+ 'and'+ num2+ 'is: '+sum);
